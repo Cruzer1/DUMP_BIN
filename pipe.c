ac12a33b24c5d2a982b68c851951933ba202bc7e
Cruzer <33875325+Cruzer1@users.noreply.github.com>
2019-02-23 10:34:57 +0530

    Create pipe.c

---
+++ pipe.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+int ipipe[2],icheck,icount=0;
+while(1)
+{
+icheck=pipe(ipipe);
+if(icheck==-1)
+{
+printf("\n\t pipe creation over\n");
+break;
+}
+icount++;
+printf("\n\t R.P.D=%d\t W.P.D=%d",ipipe[0],ipipe[1]);
+}
+printf("\n\t total number of pipes created=%d\n",icount);
+printf("\n________________________");
+printf("\n N.B");
+printf("\n\t R.P.D-> read pipe descriptor");
+printf("\n\t\ w.p.d -> write pipe desriptor\n");
+}
