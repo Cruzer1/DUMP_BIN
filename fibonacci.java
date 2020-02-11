639cebbc6b1edd46fab21a2af7f15b4c71a2af0c
Cruzer <33875325+Cruzer1@users.noreply.github.com>
2020-02-11 23:10:00 +0530

    Derp

--- fibonacci.java
+++ fibonacci.java
@@ -3,23 +3,18 @@ class palindrome
 {
 	public static void main(String args[])	
 	{
-	System.out.println("Enter a number to check : \n");
+	System.out.println("Enter the limit : \n");
 	Scanner sc=new Scanner(System.in);
 	int n=sc.nextInt();
-	int r,sum=0,temp;
-	temp=n;
-	while(n>0)
-{
-	r=n%10;
-	sum=(sum*10)+r;
-	n=n/10;
+	System.out.print("First " + n + " terms: ");
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(t1 + " + ");
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }
 }
-	if(temp==sum)
-	System.out.println("the number  is palindrome");
-	else
-	System.out.println("the number  is not palindrome");
-}
-}
-
 
 	
